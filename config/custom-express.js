5c44d6c0c136ff4741b99ec9387286c7fb028acc
isaacjamesufc <isaacjamesufc@users.noreply.github.com>
2018-11-26 16:07:17 -0300

    changed custom-express

--- config/custom-express.js
+++ config/custom-express.js
@@ -6,7 +6,7 @@ app.controlContact = require('../app/controllers/contact')
 app.controlView = require('../app/controllers/view')
 app.controlUtils = require('../app/controllers/utils')
 
-app.set("views", __dirname+"\\..\\app\\views");
+app.set("views", __dirname+"/../app/views");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
     extended:true
